9e2cf066439b338c347110943f3069b95640a701
Axel-AMGHAR <alex84250231199@gmail.com>
2022-04-14 16:05:26 +0200

    card component

---
+++ src/components/FirstStage.vue
@@ -0,0 +1,24 @@
+<template>
+  <card v-for="(action, index) of actions">{{ action.name }}</card>
+</template>
+
+<script>
+import Card from "./base/Card";
+
+export default {
+  name: "FirstStage",
+  components: {
+    Card
+  },
+  props: {
+    actions: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/base/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="border-2 border-black rounded">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
