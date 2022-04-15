82b6dde1a0fb58db6e7ddd850cf165e5e17cfb52
Celine Lieutaud <celine.lieutaud@ynov.com>
2022-04-15 17:16:36 +0200

    add MyReset component

---
+++ src/components/MyReset.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="absolute top-15 right-10">
+        <div class="border-4 rounded-md border-green-500 shadow-sm bg-slate-800 text-white text-left h-auto p-5">
+        <span>Trésors trouvés : {{ count }} / 7</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "MyReset",
+  props: {
+      count: Number
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
